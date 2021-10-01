9ad73a63b1243b23b56bc73a593fef3c136eeb51
Alexandr <43439825+Bestoloch@users.noreply.github.com>
2021-10-01 12:49:27 +0300

    Update nessus2redis.py

--- nessus2redis.py
+++ nessus2redis.py
@@ -1,30 +1,27 @@
 import io
+import time
 import json, csv
 import requests
 import urllib3
-import tempfile
-import time
-import os
 import redis
 import datetime
-import socket
-import xmltodict
 import pandas
 import hashlib
-from lxml import etree
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 url = "https://127.0.0.1:8834"
 verify = False
-access_key = "..."
-secret_key = "..."
+access_key = "...access_key..."
+secret_key = "...secret_key..."
+headers = {"X-ApiKeys": f"accessKey={access_key}; secretKey={secret_key}"}
+headers_ct = {"X-ApiKeys": f"accessKey={access_key}; secretKey={secret_key}", "Accept": "application/json", "Content-Type": "application/json"}
 
 def get(dest):
   return requests.get(f"{url}/{dest}", verify = verify, headers = {"X-ApiKeys": f"accessKey={access_key}; secretKey={secret_key}"})
 
 def post (dest, data):
-  return requests.post(f"{url}/{dest}", verify = verify, headers = {"X-ApiKeys": f"accessKey={access_key}; secretKey={secret_key}"}, data = data)
+  return requests.post("{}/{}".format(url, dest), verify = verify, headers = {"X-ApiKeys": f"accessKey={access_key}; secretKey={secret_key}"}, data = data)
 
 def json_post (dest, json):
   return requests.post(f"{url}/{dest}", verify=verify, headers = {"X-ApiKeys": f"accessKey={access_key}; secretKey={secret_key}", "Accept": "application/json", "Content-Type": "application/json"}, json = json)
@@ -33,80 +30,86 @@ r_FirstSeen = redis.Redis(db=1)
 r = redis.Redis()
 r_Status = redis.Redis(db=2)
 
-scans_request = get("scans")
+scans_request = get("scans?folder_id=17")
 scans = scans_request.json()['scans']
 
 for scan in scans:
-  scan_name = scan['name']
-  if scan['status'] == 'completed':
-    data = {
-      "format":"csv",
-      "reportContents": {
-        "csvColumns": {
-          "id": True,
-          "cve": True,
-          "cvss": False,
-          "risk": False,
-          "hostname": True,
-          "protocol": True,
-          "port": True,
-          "plugin_name": True,
-          "cvss3_base_score": True,
-          "description": True,
-          "synopsis": True,
-          "solution": True,
-          "see_also": False,
-          "plugin_output": False
+  scan_id = scan['uuid']
+  if (r_Status.exists(f"nessus_{scan_id}")):
+    pass
+  else:
+    if scan['status'] == 'completed':
+      data = {
+        "format":"csv",
+        "reportContents": {
+          "csvColumns": {
+            "id": True,
+            "cve": True,
+            "cvss": False,
+            "risk": False,
+            "hostname": True,
+            "protocol": True,
+            "port": True,
+            "plugin_name": True,
+            "cvss3_base_score": True,
+            "description": True,
+            "synopsis": True,
+            "solution": True,
+            "see_also": False,
+            "plugin_output": False
+          }
         }
       }
-    }
-    resp = json_post(f"scans/{scan['id']}/export", data)
-    export_token = json.loads(resp.text)['file']
-    resp_status = get(f"scans/{scan['id']}/export/{export_token}/status")
-    status = json.loads(resp_status.text)['status']
-    while (status != "ready"):
-      time.sleep(0.5)
+      resp = json_post(f"scans/{scan['id']}/export", data)
+      export_token = json.loads(resp.text)['file']
       resp_status = get(f"scans/{scan['id']}/export/{export_token}/status")
       status = json.loads(resp_status.text)['status']
-    if status == "ready":
-      resp_nessus = get(f"scans/{scan['id']}/export/{export_token}/download")
-      vulners = pandas.read_csv(io.StringIO(resp_nessus.text), delimiter=',')
-      vuln_dict = vulners.to_dict(orient='records')
-      for vuln in vuln_dict:
-        new_dict = {}
-        new_dict['Hostname'] = vuln['Host']
-        new_dict['Description'] = vuln['Description']
-        new_dict['NVT Name'] = vuln['Name']
-        new_dict['Port'] = int(vuln['Port'])
-        new_dict['tag'] = 'Nessus'
-        if (pandas.isna(vuln['CVSS v3.0 Base Score'])):
-          new_dict['CVSS Score'] = 0
-        else:
-          new_dict['CVSS Score'] = int(vuln['CVSS v3.0 Base Score'])
-        if (new_dict['CVSS Score'] == 0):
-          new_dict['Severity Score'] = "Info"
-        elif (new_dict['CVSS Score'] > 0) and (new_dict['CVSS Score'] < 4):
-          new_dict['Severity Score'] = "Low"
-        elif (new_dict['CVSS Score'] >= 4) and (new_dict['CVSS Score'] < 7):
-          new_dict['Severity Score'] = "Medium"
-        elif (new_dict['CVSS Score'] >= 7) and (new_dict['CVSS Score'] < 9):
-          new_dict['Severity Score'] = "High"
-        elif (new_dict['CVSS Score'] >= 9) and (new_dict['CVSS Score'] <= 10):
-          new_dict['Severity Score'] = "Critical"
-        else:
-          new_dict['Severity Score'] = "Info"
-        new_dict['Protocol'] = vuln['Protocol']
-        new_dict['Synopsis'] = vuln['Synopsis']
-        if (pandas.isna(vuln['Solution'])):
-          new_dict['Solution'] = "-"
-        else:
-          new_dict['Solution'] = vuln['Solution']
-        NVT_id = f"{new_dict['Hostname']}|{new_dict['Port']}|{new_dict['Protocol']}|{new_dict['Synopsis']}|{new_dict['Description']}"
-        q1_hash = hashlib.sha256(NVT_id.encode('utf-8')).hexdigest()
-        new_dict['ID'] = f"nessus_{q1_hash}"
-        if (r_FirstSeen.exists(new_dict['ID'])):
-          new_dict['First seen'] = r_FirstSeen.get(new_dict['ID']).decode("utf-8")
-        else:
-          r_FirstSeen.set(new_dict['ID'], datetime.date.today().strftime("%Y.%m.%d"))
-          new_dict['First seen'] = r_FirstSeen.get(new_dict['ID']).decode("utf-8")
-        r.rpush("nessus", json.dumps(new_dict)+"\n")
+      while (status != "ready"):
+        time.sleep(0.5)
+        resp_status = get(f"scans/{scan['id']}/export/{export_token}/status")
+        status = json.loads(resp_status.text)['status']
+      if status == "ready":
+        resp_nessus = get(f"scans/{scan['id']}/export/{export_token}/download")
+        vulners = pandas.read_csv(io.StringIO(resp_nessus.text), delimiter=',')
+        vulners = vulners[vulners['Plugin ID'] != 19506]
+        vulners.rename(columns={'Host':'Hostname', 'Name': 'NVT Name', 'CVSS v3.0 Base Score': 'CVSS Score'}, inplace = True)
+        vulners['CVSS Score'] = vulners['CVSS Score'].fillna(0)
+        vulners['Solution'] = vulners['Solution'].fillna('-')
+        vulners.loc[(vulners['CVSS Score'] == 0), 'Severity Score'] = 'Info'
+        vulners.loc[(vulners['CVSS Score'] > 0) & (vulners['CVSS Score'] < 4), 'Severity Score'] = 'Low'
+        vulners.loc[(vulners['CVSS Score'] >= 4) & (vulners['CVSS Score'] < 7), 'Severity Score'] = 'Medium'
+        vulners.loc[(vulners['CVSS Score'] >= 7) & (vulners['CVSS Score'] < 9), 'Severity Score'] = 'High'
+        vulners.loc[(vulners['CVSS Score'] >= 9) & (vulners['CVSS Score'] <= 10), 'Severity Score'] = 'Critical'
+        vulners[['tag', 'Company']] = 'Nessus', Company
+        vulners['Port'] = vulners['Port'].astype(int)
+        vulners['NetWeb_type'] = 'Net'
+        vulners.loc[(vulners['Plugin ID'] == 11219), 'NetWeb_type'] = 'Net'
+
+        net_vuln = vulners[vulners['NetWeb_type'] == 'Net'].copy()
+        net_vuln['ID'] = net_vuln['Hostname'].astype(str)+'|'+net_vuln['Port'].astype(str)+'|'+net_vuln['Protocol'].astype(str)+'|'+net_vuln['Synopsis'].astype(str)+'|'+net_vuln['Description'].astype(str)
+        net_vuln['ID'] = net_vuln['ID'].apply(lambda x: f"nessus_{hashlib.sha256(x.encode('utf-8')).hexdigest()}")
+        net_vuln['First seen'] = datetime.date.today().strftime("%Y.%m.%d")
+        for id in net_vuln['ID']:
+          if (r_FirstSeen.exists(id)):
+            pass
+          else:
+            r_FirstSeen.set(id, datetime.date.today().strftime("%Y.%m.%d"))
+        net_vuln['First seen'] = net_vuln['ID'].apply(lambda x, y=r_FirstSeen: y.get(x).decode("utf-8"))
+        for vuln in net_vuln[['ID', 'Hostname', 'Port', 'Protocol', 'NVT Name', 'Synopsis', 'Description', 'CVSS Score', 'Severity Score', 'Solution', 'First Seen', 'tag']].to_dict(orient='records'):
+          r.rpush("net", json.dumps(vuln))
+'''
+        web_vuln = vulners[vulners['NetWeb_type'] == 'Web'].copy()
+        web_vuln.rename(columns={'Synopsis': 'Details', 'Solution': 'Recomendation'}, inplace = True)
+        web_vuln['ID'] = web_vuln['Company'].astype(str)+'|'+web_vuln['Hostname'].astype(str)+'|'+web_vuln['Port'].astype(str)+'|'+web_vuln['Details'].astype(str)+'|'+web_vuln['Description'].astype(str)
+        web_vuln['ID'] = web_vuln['ID'].apply(lambda x: f"nessus_{hashlib.sha256(x.encode('utf-8')).hexdigest()}")
+        web_vuln['First seen'] = datetime.date.today().strftime("%Y.%m.%d")
+        for id in web_vuln['ID']:
+          if (r_FirstSeen.exists(id)):
+            pass
+          else:
+            r_FirstSeen.set(id, datetime.date.today().strftime("%Y.%m.%d"))
+        web_vuln['First seen'] = web_vuln['ID'].apply(lambda x, y=r_FirstSeen: y.get(x).decode("utf-8"))
+        for vuln in web_vuln[['ID', 'Hostname', 'Port', 'NVT Name', 'Details', 'Description', 'CVSS Score', 'Severity Score', 'Recomendation', 'First seen', 'tag']].to_dict(orient='records'):
+          r.rpush("web_nessus", json.dumps(vuln))
+'''
+      r_Status.set(f"nessus_{scan_id}", datetime.date.today().strftime("%Y.%m.%d"))
